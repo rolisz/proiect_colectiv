c039ae48880ae2fe81f32c1a78a20d58922f4645
Roland <rolisz@yahoo.com>
2013-04-13 19:48:02 +0300

    Adaugat functionalitatea de importare orar si mai frecat un pic modelul pentru orar.

--- rms/Model/Orar.py
+++ rms/Model/Orar.py
@@ -2,27 +2,58 @@ from sqlalchemy.schema import Column
 import sqlalchemy.types
 from camelot.admin.entity_admin import EntityAdmin
 from camelot.core.orm import Entity
-from sqlalchemy import Unicode, Date, Integer, Boolean
+from sqlalchemy import Unicode, Date, Integer, Boolean, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import ForeignKey
 
+zile = {'Luni': 1, 'Marti': 2, 'Miercuri': 3, 'Joi': 4, ' Vineri': 5}
+
 class Orar(Entity):
+    __tablename__ = 'orar'
 
-	__tablename__ = 'orar'
+    zi = Column(Integer, nullable=False)
+    ora = Column(String(30), nullable=False)
+    frecventa = Column(Integer, nullable=False)
+    sala = Column(String(6), nullable=False)
+    anul = Column(String(10), nullable=False)
+    formatia = Column(String(5), nullable=False)
+    tip = Column(Integer, nullable=False)
+    disciplina = Column(String(30), nullable=False)
 
-	zi = Column(Integer, nullable=False)
-	ora = Column(Unicode(30),nullable=False)
-	frecventa = Column(Integer, nullable=False)
-	sala = Column(Unicode(6), nullable=False)
-	anul = Column(Unicode(10), nullable=False)
-	formatia = Column(Unicode(5), nullable=False)
-	tip = Column(Integer, nullable=False)
-	disciplina = Column(Unicode(30), nullable=False)
+    def __init__(self, zi=None, ora=None, frecventa=None, sala=None,
+                 anul=None, formatia=None, tip=None, disciplina=None):
+        if zi:
+            self.zi = zile[zi]
+            self.ora = ora
+            self.frecventa = frecventa
+            self.sala = sala
+            self.anul = anul
+            self.formatia = formatia
+            if tip == 'L':
+                self.tip = 1
+            elif tip == 'S':
+                self.tip = 2
+            elif tip == 'C':
+                self.tip = 3
+            else:
+                raise Exception("Tip de ora invalid")
+            self.disciplina = disciplina
 
-	def __unicode__(self):
-		return self.sala or 'Unknown'
 
-	class Admin(EntityAdmin):
-		verbose_name = 'Orar'
-		verbose_name_plural = 'Orar'
-		list_display = ['zi','ora','frecventa','sala','anul','formatia','tip','disciplina']
+    def __unicode__(self):
+        return self.sala or 'Unknown'
+
+    class Admin(EntityAdmin):
+        verbose_name = 'Orar'
+        verbose_name_plural = 'Orar'
+        list_display = ['zi', 'ora', 'frecventa', 'sala', 'anul', 'formatia', 'tip', 'disciplina']
+        field_attributes = {
+            'zi': {'choices': lambda o: [(v,k) for k,v in zile.items()]
+            },
+            'frecventa': {'choices': lambda o: [(0, 'Saptamanal'),
+                                                (1, 'Saptamana para'),
+                                                (2, 'Saptamana impara')]},
+            'tip': {'choices': lambda o: [(1, 'Laborator'),
+                                          (2, 'Seminar'),
+                                          (3, 'Curs')]}
+        }
---
+++ rms/Views/import_orar.py
@@ -0,0 +1,47 @@
+__author__ = 'Roland'
+
+from camelot.admin.action import Action
+
+
+class ImportException(Exception):
+    pass
+
+
+class ImportOrar(Action):
+    verbose_name = 'Importare orar'
+
+    def model_run(self, model_context):
+        from camelot.view.action_steps import (SelectFile,
+                                               UpdateProgress,
+                                               Refresh,
+                                               FlushSession)
+
+        select_files = SelectFile('Txt Files (*.txt *.csv);;All Files (*)')
+        select_files.single = False
+        file_names = yield select_files
+        file_count = len(file_names)
+
+        import os
+        from camelot.core.orm import Session
+        from rms.Model.Orar import Orar
+
+        session = Session()
+
+        for i, file_name in enumerate(file_names):
+            yield UpdateProgress(i, file_count)
+            title = os.path.splitext(os.path.basename(file_name))[0]
+            print(file_name)
+            for line in open(file_name):
+                vals = line.split(';')[:-1]
+                if len(vals) != 8:
+                    raise ImportException("Fisierul nu e dat bine")
+                if vals[2] not in ['0', '1', '2']:
+                    raise ImportException("Frecventa nu e data bine")
+                print(vals)
+                orar = Orar(*vals)
+                print(orar)
+                session.add(orar)
+        yield FlushSession(session)
+        # begin refresh
+        yield Refresh()
+        # end refresh
